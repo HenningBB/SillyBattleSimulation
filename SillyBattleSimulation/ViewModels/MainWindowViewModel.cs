58687781749d7a4eb0dbb69937505431deca619e
HenningBB <henning.bull-berg@tsbw.cloud>
2020-06-24 14:50:09 +0200

    Connected the ViewModel Properties and Commands to the View

--- SillyBattleSimulation/ViewModels/MainWindowViewModel.cs
+++ SillyBattleSimulation/ViewModels/MainWindowViewModel.cs
@@ -9,17 +9,65 @@ namespace SillyBattleSimulation.ViewModels
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Windows.Input;
+    using SillyBattleSimulation.Commands;
+    using SillyBattleSimulation.Models;
 
     /// <summary>
     /// Class of the Main View.
     /// </summary>
     public class MainWindowViewModel : BaseViewModel
     {
+        private TeamModel teamA;
+        private TeamModel teamB;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
         /// </summary>
         public MainWindowViewModel()
         {
+            this.teamA = new TeamModel();
+            this.teamB = new TeamModel();
+            this.AddWarriorACommand = new Command(this.AddWarriorA);
+            this.AddWarriorBCommand = new Command(this.AddWarriorB);
+        }
+
+        /// <summary>
+        /// Gets the Command to add an Warrior to TeamA.
+        /// </summary>
+        public ICommand AddWarriorACommand { get; private set; }
+
+        /// <summary>
+        /// Gets the Command to add an Warrior to Teamb.
+        /// </summary>
+        public ICommand AddWarriorBCommand { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the Model of the first Team.
+        /// </summary>
+        public TeamModel TeamA
+        {
+            get => this.teamA;
+            set => this.SetProperty(ref this.teamA, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the Model of the second Team.
+        /// </summary>
+        public TeamModel TeamB
+        {
+            get => this.teamB;
+            set => this.SetProperty(ref this.teamB, value);
+        }
+
+        private void AddWarriorA(object commandParameter)
+        {
+            this.TeamA.AddRandomWarrior();
+        }
+
+        private void AddWarriorB(object commandParameter)
+        {
+            this.TeamB.AddRandomWarrior();
         }
     }
 }
