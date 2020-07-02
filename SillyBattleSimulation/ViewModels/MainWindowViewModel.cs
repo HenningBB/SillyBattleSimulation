490af3489f91ac0ea72928c0fbb9970eebe5f5fe
HenningBB <henning.bull-berg@tsbw.cloud>
2020-07-02 15:15:08 +0200

    Changed the MainViewModel to be able to change the showed ViewModel

--- SillyBattleSimulation/ViewModels/MainWindowViewModel.cs
+++ SillyBattleSimulation/ViewModels/MainWindowViewModel.cs
@@ -16,34 +16,47 @@ namespace SillyBattleSimulation.ViewModels
     /// </summary>
     public class MainWindowViewModel : BaseViewModel
     {
-        private object simulation;
-        private object visualSimulation;
+        private BaseViewModel currentViewModel;
+        private bool change;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
         /// </summary>
         public MainWindowViewModel()
         {
-            this.Simulation = new SimulationViewModel();
-            this.VisualSimulation = new VisualSimulationViewModel();
+            this.CurrentViewModel = new SimulationViewModel();
+
+            this.ChangeViewmodelCommand = new Command(this.ChangeViewModel);
+
+            this.change = false;
         }
+
+        /// <summary>
+        /// Gets the Command to change the current Viewmodel.
+        /// </summary>
+        public ICommand ChangeViewmodelCommand { get; private set; }
 
         /// <summary>
         /// Gets or sets the currently selected Viewmodel.
         /// </summary>
-        public object Simulation
+        public BaseViewModel CurrentViewModel
         {
-            get => this.simulation;
-            set => this.SetProperty(ref this.simulation, value);
+            get => this.currentViewModel;
+            set => this.SetProperty(ref this.currentViewModel, value);
         }
 
-        /// <summary>
-        /// Gets or sets the currently selected Viewmodel.
-        /// </summary>
-        public object VisualSimulation
+        private void ChangeViewModel(object commandParameter)
         {
-            get => this.visualSimulation;
-            set => this.SetProperty(ref this.visualSimulation, value);
+            if (!this.change)
+            {
+                this.CurrentViewModel = new VisualSimulationViewModel();
+            }
+            else
+            {
+                this.CurrentViewModel = new SimulationViewModel();
+            }
+
+            this.change = !this.change;
         }
     }
 }
