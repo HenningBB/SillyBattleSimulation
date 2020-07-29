cff2df66ffbef0eeafd85bc5c2c3f7654d2931bf
HenningBB <henning.bull-berg@tsbw.cloud>
2020-07-29 11:29:48 +0200

    Created a small Graph

--- SillyBattleSimulation/ViewModels/CoolGraphViewModel.cs
+++ SillyBattleSimulation/ViewModels/CoolGraphViewModel.cs
@@ -9,6 +9,9 @@ namespace SillyBattleSimulation.ViewModels
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Shapes;
 
     /// <summary>
     /// Class of the CollGraphView.
@@ -16,12 +19,16 @@ namespace SillyBattleSimulation.ViewModels
     public class CoolGraphViewModel : BaseViewModel
     {
         private string test;
+        private PointCollection polo;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CoolGraphViewModel"/> class.
         /// </summary>
         public CoolGraphViewModel()
         {
+            this.Polo = new PointCollection();
+            this.Polo.Add(new Point(10, 10));
+            this.Polo.Add(new Point(20, 20));
             this.Test = "Hallo Welt";
         }
 
@@ -29,6 +36,12 @@ namespace SillyBattleSimulation.ViewModels
         {
             get => this.test;
             set => this.SetProperty(ref this.test, value);
+        }
+
+        public PointCollection Polo
+        {
+            get => this.polo;
+            set => this.SetProperty(ref this.polo, value);
         }
     }
 }
