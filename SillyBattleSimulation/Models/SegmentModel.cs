55d114609db55c7e5cc457a5556cebc5bda70419
HenningBB <henning.bull-berg@tsbw.cloud>
2020-07-30 09:13:34 +0200

    Made the Graph show a Line that always stays in the given Border

---
+++ SillyBattleSimulation/Models/SegmentModel.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="SegmentModel.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SillyBattleSimulation.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    public class SegmentModel : BaseModel
+    {
+        private Point from;
+        private Point to;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SegmentModel"/> class.
+        /// </summary>
+        public SegmentModel(Point from,Point to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+
+        public Point From
+        {
+            get => this.from;
+            set => this.SetProperty(ref this.from, value);
+        }
+
+        public Point To
+        {
+            get => this.to;
+            set => this.SetProperty(ref this.to, value);
+        }
+    }
+}
--- SillyBattleSimulation/ViewModels/CoolGraphViewModel.cs
+++ SillyBattleSimulation/ViewModels/CoolGraphViewModel.cs
@@ -6,12 +6,16 @@ namespace SillyBattleSimulation.ViewModels
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Input;
     using System.Windows.Media;
     using System.Windows.Shapes;
+    using SillyBattleSimulation.Commands;
+    using SillyBattleSimulation.Models;
 
     /// <summary>
     /// Class of the CollGraphView.
@@ -19,18 +23,31 @@ namespace SillyBattleSimulation.ViewModels
     public class CoolGraphViewModel : BaseViewModel
     {
         private string test;
-        private PointCollection polo;
+        private int updateX;
+        private int updateY;
+        private ObservableCollection<SegmentModel> polo;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CoolGraphViewModel"/> class.
         /// </summary>
         public CoolGraphViewModel()
         {
-            this.Polo = new PointCollection();
-            this.Polo.Add(new Point(10, 10));
-            this.Polo.Add(new Point(20, 20));
+            this.GraphHeigth = 200; // inversed
+            this.GraphWidth = 200;
+            this.Polo = new ObservableCollection<SegmentModel>();
+            this.Polo.Add(new SegmentModel(new Point(0, this.GraphHeigth), new Point(10, this.GraphHeigth)));
+            this.updateX = 10;
+            this.updateY = this.GraphHeigth;
             this.Test = "Hallo Welt";
+            this.MathCommand = new Command(this.Mather);
+            this.MathicCommand = new Command(this.Mathic);
+            this.MathixCommand = new Command(this.Mathix);
+            this.MathoxCommand = new Command(this.Mathox);
         }
+
+        public int GraphWidth { get; private set; }
+
+        public int GraphHeigth { get; private set; }
 
         public string Test
         {
@@ -38,10 +55,75 @@ namespace SillyBattleSimulation.ViewModels
             set => this.SetProperty(ref this.test, value);
         }
 
-        public PointCollection Polo
+        public ObservableCollection<SegmentModel> Polo
         {
             get => this.polo;
             set => this.SetProperty(ref this.polo, value);
+        }
+
+        public ICommand MathCommand { get; private set; }
+
+        public ICommand MathicCommand { get; private set; }
+
+        public ICommand MathixCommand { get; private set; }
+
+        public ICommand MathoxCommand { get; private set; }
+
+        private void Mather(object commandParameter)
+        {
+            this.UpdateGraph(10);
+        }
+
+        private void Mathic(object commandParameter)
+        {
+            this.UpdateGraph(20);
+        }
+
+        private void Mathix(object commandParameter)
+        {
+            this.UpdateGraph(-10);
+        }
+
+        private void Mathox(object commandParameter)
+        {
+            this.UpdateGraph(-20);
+        }
+
+        private void UpdateGraph(int yChange)
+        {
+            // int minX = 0; not needed, because it is the starting Point
+            int maxX = this.GraphWidth;
+            int minY = 0;
+            int maxY = this.GraphHeigth;
+
+            this.updateX += 10;
+            this.updateY += yChange;
+
+            if (this.updateX > maxX)
+            {
+                this.Polo.Remove(this.Polo[0]);
+                foreach (var item in this.Polo)
+                {
+                    item.From = new Point(item.From.X - 10, item.From.Y);
+                    item.To = new Point(item.To.X - 10, item.To.Y);
+                }
+
+                this.updateX = maxX;
+            }
+
+            if (this.updateY < minY && yChange < 0)
+            {
+                this.updateY = minY;
+            }
+
+            if (this.updateY > maxY && yChange > 0)
+            {
+                this.updateY = maxY;
+            }
+
+            this.Polo.Add(new SegmentModel(this.Polo[this.Polo.Count - 1].To, new Point(this.updateX, this.updateY)));
+
+            // this.Test = this.updateY.ToString();
         }
     }
 }
